338dae3a03bc3adea103b1631d1208ec3164a096
EnjoyW0rld <denisavatar@hotmail.com>
2023-02-10 14:53:00 +0100

    On before placement change

--- Assets/Scripts/BuildManager.cs
+++ Assets/Scripts/BuildManager.cs
@@ -1,18 +1,117 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BuildManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private GridMap _gridMap;
+    private Tower _currentTower;
+
+    [SerializeField] private SingleTargetTower _sniperTower;
+    [SerializeField] private SlowingTower _slowingTower;
+    [SerializeField] private AOE_Tower _aoeTower;
+
+    private List<Tower> _placedTowers;
+    private GameManager _gameManager;
+    private EventManager _eventManager;
+
+    private void Start()
     {
-        
+        _placedTowers = new List<Tower>();
+        _gridMap = FindObjectOfType<GridMap>();
+        _gameManager = FindObjectOfType<GameManager>();
+        _eventManager = FindObjectOfType<EventManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void BuildPhaseManager()
     {
-        
+        Vector3 mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        mousePos.z = 0;
+
+        mousePos = _gridMap.GetPosAtGridCenter(mousePos);
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            if (_currentTower != null) _currentTower = null;
+            _currentTower = Instantiate(_sniperTower, mousePos, Quaternion.identity).GetComponent<Tower>();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            if (_currentTower != null) _currentTower = null;
+            _currentTower = Instantiate(_slowingTower, mousePos, Quaternion.identity).GetComponent<Tower>();
+        }
+        else if (Input.GetKeyDown(KeyCode.Delete))
+        {
+            Tower tower = GetTowerOnPos(mousePos);
+            if (_currentTower != null)
+            {
+                Destroy(_currentTower.gameObject);
+                _currentTower = null;
+            }
+            else if (tower != null)
+            {
+                _gridMap.FreeGridCell(tower.transform.position);
+                Destroy(tower.gameObject);
+                _placedTowers.Remove(tower);
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Tower tower = GetTowerOnPos(mousePos);
+            print(tower);
+            if (tower != null)
+            {
+                int price = tower.LvlUpPrice();
+                int moneyAmount = _gameManager.GetMoneyAmount();
+                if (price <= moneyAmount && price != -1)
+                {
+                    tower.LvlUp();
+                    moneyAmount -= price;
+                    _eventManager.OnMoneyChange?.Invoke(moneyAmount);
+                }
+            }
+        }
+
+        if (_currentTower == null) return;
+
+        _currentTower.transform.position = mousePos;
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (_gridMap.isCellEmpty(mousePos))
+            {
+                _gridMap.OccupyGridCell(mousePos);
+                _placedTowers.Add(_currentTower);
+                _currentTower = null;
+            }
+        }
+
     }
+
+    private Tower GetTowerOnPos(Vector3 pos)
+    {
+        if (_placedTowers.Count == 0) return null;
+        for (int i = 0; i < _placedTowers.Count; i++)
+        {
+            if (_placedTowers[i].transform.position == pos)
+            {
+                return _placedTowers[i];
+            }
+        }
+        return null;
+    }
+    public List<Tower> GetPlacedTowers() => _placedTowers;
+
+}
+
+[Serializable]
+class TowerContainer
+{
+    public GameObject _towerPrefab;
+    public Button _selectButton;
+    public TextMeshProUGUI _priceText; 
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,16 +9,11 @@ public class GameManager : MonoBehaviour
 
     private GamePhase _gamePhase = GamePhase.Build;
 
-    private List<Tower> _placedTowers;
-    private Tower _currentTower;
     private WaveManager _waveManager;
     private EventManager _eventManager;
-    private GridMap _gridMap;
+    private BuildManager _buildManager;
+    //private GridMap _gridMap;
     private int _moneyAmount = 50;
-
-    [SerializeField] private GameObject _sniperTower;
-    [SerializeField] private GameObject _aoeTower;
-    [SerializeField] private GameObject _slowingTower;
 
     private void Start()
     {
@@ -26,10 +21,9 @@ public class GameManager : MonoBehaviour
         _eventManager.OnEnemyDeath.AddListener(UpdateMoneyAmount);
         _eventManager.OnPhaseChange.AddListener(OnPhaseChange);
 
-        _placedTowers = new List<Tower>();
         _waveManager = FindObjectOfType<WaveManager>();
+        _buildManager = FindObjectOfType<BuildManager>();
 
-        _gridMap = FindObjectOfType<GridMap>();
     }
 
     private void Update()
@@ -40,7 +34,7 @@ public class GameManager : MonoBehaviour
                 AttackPhaseManager();
                 break;
             case GamePhase.Build:
-                BuildPhaseManager();
+                _buildManager.BuildPhaseManager();
                 break;
 
         }
@@ -52,90 +46,18 @@ public class GameManager : MonoBehaviour
 
     private void AttackPhaseManager()
     {
-        for (int i = 0; i < _placedTowers.Count; i++)
+        List<Tower> placedTowers = _buildManager.GetPlacedTowers();
+        for (int i = 0; i < placedTowers.Count; i++)
         {
-            if (!_placedTowers[i].canShoot) continue;
-            if (_waveManager.TryAttack(_placedTowers[i]))
+            if (!placedTowers[i].canShoot) continue;
+            if (_waveManager.TryAttack(placedTowers[i]))
             {
-                _placedTowers[i].UpdateCooldown();
+                placedTowers[i].UpdateCooldown();
             }
         }
     }
 
-    private void BuildPhaseManager()
-    {
-        Vector3 mousePos = Input.mousePosition;
-        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
-        mousePos.z = 0;
 
-        mousePos = _gridMap.GetPosAtGridCenter(mousePos);
-
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            _currentTower = Instantiate(_sniperTower, mousePos, Quaternion.identity).GetComponent<Tower>();
-        }else if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            _currentTower = Instantiate(_slowingTower, mousePos, Quaternion.identity).GetComponent<Tower>();
-        }
-        else if (Input.GetKeyDown(KeyCode.Delete))
-        {
-            Tower tower = GetTowerOnPos(mousePos);
-            if(_currentTower != null)
-            {
-                Destroy(_currentTower.gameObject);
-                _currentTower = null;
-            }
-            else if (tower != null)
-            {
-                _gridMap.FreeGridCell(tower.transform.position);
-                Destroy(tower.gameObject);
-                _placedTowers.Remove(tower);
-            }
-        }
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            Tower tower = GetTowerOnPos(mousePos);
-            print(tower);
-            if (tower != null)
-            {
-                int price = tower.LvlUpPrice();
-                if (price <= _moneyAmount && price != -1)
-                {
-                    tower.LvlUp();
-                    _moneyAmount -= price;
-                    _eventManager.OnMoneyChange?.Invoke(_moneyAmount);
-                }
-            }
-        }
-
-        if (_currentTower == null) return;
-
-        _currentTower.transform.position = mousePos;
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (_gridMap.isCellEmpty(mousePos))
-            {
-                _gridMap.OccupyGridCell(mousePos);
-                _placedTowers.Add(_currentTower);
-                _currentTower = null;
-            }
-        }
-
-    }
-
-    private Tower GetTowerOnPos(Vector3 pos)
-    {
-        if (_placedTowers.Count == 0) return null;
-        for (int i = 0; i < _placedTowers.Count; i++)
-        {
-            if (_placedTowers[i].transform.position == pos)
-            {
-                return _placedTowers[i];
-            }
-        }
-        return null;
-    }
 
     private void OnPhaseChange(GamePhase phase)
     {
@@ -146,4 +68,5 @@ public class GameManager : MonoBehaviour
         _moneyAmount += enemy.GetMoney();
         _eventManager.OnMoneyChange?.Invoke(_moneyAmount);
     }
+    public int GetMoneyAmount() => _moneyAmount;
 }
